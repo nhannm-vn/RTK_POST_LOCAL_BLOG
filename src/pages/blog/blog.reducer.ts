0d9def46816eace4536c95ec9d218df4b9ad9acb
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-01-01 17:18:19 +0700

    update action delete post

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -12,18 +12,35 @@ const initalState: BlogState = {
   postList: initalPostList
 }
 
-// Tạo action
+// Tạo action:
+// Thêm
 export const addPost = createAction<Post>('blog/addPost')
+
+// Xóa
+export const deletePost = createAction<string>('blog/deletePost')
 
 //<Post>: là định dạng cho kiểu dữ liệu gửi lên payload
 
 const blogReducer = createReducer(initalState, (builder) => {
-  builder.addCase(addPost, (state, action) => {
-    // immerjs
-    // immerjs: giúp cho chúng ta mutate một state an toàn
-    const post = action.payload
-    state.postList.push(post)
-  })
+  builder
+    .addCase(addPost, (state, action) => {
+      // immerjs
+      // immerjs: giúp cho chúng ta mutate một state an toàn
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      // Lấy id từ action. Id nằm trong payload của action
+      const postId = action.payload
+      // Tìm vịt trí của thằng muốn xóa
+      const foundPostIndex = state.postList.findIndex((item) => item.id === postId)
+      // Xóa nó theo kiểu mutate luôn
+      //*Lưu ý trường hợp không tìm thấy. Nên cần thêm cái if
+      //  để chắc chắn rằng tìm thấy
+      if (foundPostIndex !== -1) {
+        state.postList.slice(foundPostIndex, 1)
+      }
+    })
 })
 
 export default blogReducer
